667f504fe177febd69fb877fc958bc5f0c181407
dlhtx <949729789@qq.com>
2022-09-02 17:06:06 +0800

    modified:   deploy/000-deploy-contract.ts

--- deploy/000-deploy-contract.ts
+++ deploy/000-deploy-contract.ts
@@ -1,14 +1,12 @@
 module.exports = async ({ ethers, getNamedAccounts, deployments }) => {
   const { log } = deployments
-  log(123)
-  console.log(123)
   const { deploy } = deployments
 
   const namedAccounts = await getNamedAccounts()
   log(namedAccounts)
   const { deployer } = namedAccounts
 
-  const deployResult = await deploy("ExampleToken", {
+  const deployResult = await deploy("NFTokenMetaEnumerable", {
     from: deployer,
     args: [],
     log: true,
@@ -16,8 +14,8 @@ module.exports = async ({ ethers, getNamedAccounts, deployments }) => {
   })
   if (deployResult.newlyDeployed) {
     log(
-      `contract ExampleToken deployed at ${deployResult.address} using ${deployResult.receipt.gasUsed} gas`
+      `contract NFTokenMetaEnumerable deployed at ${deployResult.address} using ${deployResult.receipt.gasUsed} gas`
     )
   }
 }
-module.exports.tags = ["ExampleToken"]
+module.exports.tags = ["NFTokenMetaEnumerable"]
